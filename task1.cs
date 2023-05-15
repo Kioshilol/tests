0bc96c9b8e86ce38e3b9fbe3246b66ebc5c7f9dd
Pavel <48092890+Kioshilol@users.noreply.github.com>
2023-05-15 22:55:49 +0200

    test1

---
+++ task1.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SystemTask = System.Threading.Tasks.Task;
+
+var taskCompiler = new TaskCompiler();
+taskCompiler.Add(() => CreateTask(1000));
+taskCompiler.Add(() => CreateTask(2000));
+taskCompiler.Add(() => CreateTask(3000));
+taskCompiler.Add(() => CreateTask(4000));
+await taskCompiler.Compile(true);
+Console.ReadLine();
+
+void CreateTask(int delay)
+{
+    Thread.Sleep(delay);
+    Console.WriteLine(delay);
+}
+
+public class TaskCompiler
+{
+    private readonly List<Task> _tasks;
+
+    public TaskCompiler()
+    {
+        _tasks = new List<Task>();
+    }
+
+    public delegate void Task();
+
+    public void Add(Task task)
+    {
+        _tasks.Add(task);
+    }
+
+    public SystemTask Compile(bool waitAll)
+    {
+        if (!waitAll)
+        {
+            _tasks.ForEach(task => SystemTask.Run(() => InternalCompile(task)));
+            return SystemTask.CompletedTask;
+        }
+        
+        var tasks = _tasks.Select(task => SystemTask.Run(() => InternalCompile(task)));
+        Console.WriteLine("Wait for all tasks");
+        return SystemTask.WhenAll(tasks);
+    
+        void InternalCompile(Task task)
+        {
+            task.Invoke();
+        }
+    }
+}
