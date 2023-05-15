ac8f4c6c116f64831a2b57f7749045ab5a47407b
Pavel <48092890+Kioshilol@users.noreply.github.com>
2023-05-16 00:02:33 +0200

    test2

---
+++ task2.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var targetRow = GetRowSums(new[] { 1d, 2d, 3d, 4d, 5d, 9 });
+Console.ReadLine();
+
+IEnumerable<double> GetRowSums(IEnumerable<double> row)
+{
+    var rowCount = row.Count();
+    var array = new double[rowCount];
+    for (var i = 0; i < rowCount; i++)
+    {
+        array[i] = row.ElementAt(i);
+        yield return array.Sum();
+    }
+}
+
